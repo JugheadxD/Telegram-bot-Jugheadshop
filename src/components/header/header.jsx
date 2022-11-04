b94aa1f45e21d59e03efbeca381c4fd0681c0045
Александр Кобалия <jughead@MacBook-Air-Aleksandr.local>
2022-11-04 14:41:26 +0300

    1

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTelegram } from "../../hooks/useTelegram";
-import Button from "../button/Button";
+import Button from "../button/button";
 
 const Header = () => {
     const { user, onClose } = useTelegram();
