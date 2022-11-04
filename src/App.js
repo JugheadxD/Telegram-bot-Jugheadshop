0b65e2c01c36cadcc66b9be79fbbb95a8a91bb0e
Александр Кобалия <jughead@MacBook-Air-Aleksandr.local>
2022-11-04 15:06:10 +0300

    132

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
 import Header from './components/Header/Header';
